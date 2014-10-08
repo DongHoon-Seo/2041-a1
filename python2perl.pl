25b974fbec014817cacb7733ff9a8beb3a590537
DongHoon-Seo <postformer@gmail.com>
2014-10-08 04:28:44 -0700

    finished all of subset2 and polished

--- python2perl.pl
+++ python2perl.pl
@@ -19,21 +19,22 @@ sub format_Perl {
 			if ($word =~ m/.*".*/g) {
 				#print "word: found \"\n";
 				next;
-			}  elsif ($word =~ /\bbreak\b/) {
+			}  elsif ($word =~ /\bbreak\b/i) {
 				#HANDLES BREAKS
 				$lines =~ s/$word/last/g;
+			} elsif ($word =~ /\bcontinue\b/i) {
+				$lines =~ s/$word/continue/g;
 			} elsif ($word !~ /\bprint\b/gi) {
-				#if($word =~ \\)
-				if($word =~ m/[A-Z]+/gi) {
+				if($word =~ m/\band\b|\bor\b|\bnot\b/gi) {
+					#does nothing if and or not come up as it is the same in both
+				} elsif($word =~ m/[A-Z]+/gi ) {
 					$word =~ s/\W*//gi;
 					$wordPl = makeVariable($word);
 					#print "word: $word wordpl: $wordPl\n";
 					#print "line:$line\n";
 					if ($lines !~ /\$+$word/) {
 						$lines =~ s/\b\$*$word\b/$wordPl/g;
-					} #
-					#$lines =~ s/\Q$word/$wordPl/g;
-					#print "wordplline = $lines\n";
+					} 
 				}
 			}
 		}
@@ -51,7 +52,7 @@ sub format_Perl {
 		if ($string =~ m/".*"/g) {
 			#print "YO\n";
 			$plString = $string.", \"\\n\"";
-		} elsif ($string =~ m/.*\*|\+.*/g) {
+		} elsif ($string =~ m/.*\*|\+|\/|\-|\%|\*\*.*/g) {
 			#print "found *\n";
  			$plString = $string.", \"\\n\"";
 		} elsif ($string =~ m/.*/g) {
@@ -73,27 +74,6 @@ sub makeVariable {
 	return $plVar;
 }
 
-sub meticOperators {
-	my ($pyLine) = @_;
-	chomp $pyLine;
-	$plLine = $pyLine;
-	@strings = split(/\s+/,$pyLine); 
-
-	foreach $word (@strings) {	
-		if($word =~ m/[A-Z]+/gi) {
-			$wordPl = makeVariable($word);
-			$plLine =~ s/$word/$wordPl/g;
-		} elsif ($word =~ m/\-[0-9]+/g) {
-			$wordPl = $word;
-			$wordPl =~ s/\-//g;
-			$plLine =~ s/$word/\- $wordPl/g;
-		}
-	}
-	
-	return $plLine.";\n";
-}
-
-
 while ($line = <>) {
 
 #	print "BEFORE:LINES $line\n";
@@ -107,8 +87,11 @@ while ($line = <>) {
 	} elsif ($line =~ /^\s*$/) {
 		
 		# If empty line ignore.
-
 		next;
+
+	} elsif ($line =~ /^#/) { 
+	
+		print $line;
 
 	} elsif ($line !~ /^\s*$/) {
 
@@ -210,87 +193,6 @@ while ($line = <>) {
 		
 		print "#$line\n";
 	}
-
-	#} #elsif ($line =~ /^\s*print\s*(.*)\s*$/) {
-	
-		# Python's print print a new-line character by default
-		# so we need to add it explicitly to the Perl print statement
-		#print "print \"\$$1\\n";
-		#print "\";\n";
-		#$plLine = format_Perl($line);
-		#print "$plLine";
-
-	#} #elsif ($line =~ /^\s*[A-Z]+[0-9]*\s*=\s*[0-9]*\s*/gi) {
-		# Variables added in; Python has no $ where as Perl does.
-		# $strings = all the seperate words from the input line
-		# $word = single word $wordpl = word in perl format
-		# $perled = string has been changed to perl format;
- 
-	#	chomp $line;
-	#	$perled = $line;
-	#	@strings = split(/\s+/,$perled); 
-
-	#	foreach $word (@strings) {	
-	#		if($word =~ m/[A-Z]+/gi) {
-	#			$wordPl = makeVariable($word);
-	#			$perled =~ s/$word/$wordPl/g;
-	#		} elsif ($word =~ m/\-[0-9]+/g) {
-	#			$wordPl = $word;
-	#			$wordPl =~ s/\-//g;
-	#			$perled =~ s/$word/\- $wordPl/g;
-	#		}
-	#	}
-	
-		#print "$perled;\n";
-#	
-	#}  elsif ($line =~ m/\s*if\s*[A-Z]+[0-9]*.*:.*/gi) { 
-
-		# Reconstructing string when single if expression is found		
-
-	#	print "Found if statement\n";
-	#	chomp $line;
-	#	$expr = $line;
-	#	$expr =~ s/if\s*//g;
-	#	$expr =~ s/\s*:.*//g;
-#
-	#	if ($expr =~ m/\(*[A-Z]+[0-9]*\s*and|or|not\s*[A-Z]+[0-9]*\)*/gi) { 
-			# This section deals with logical operators 
-			# This section should trigger when a variable and a logical operator exist
-			# on the same line.		
-	#		@strings = split(/\s+/,$expr);
-	#		foreach $word (@strings) {
-	#			if(($word =~ m/[A-Z]+[0-9]*/gi) && ($word !~ "and|or|not")) {
-	#				$wordPl = makeVariable($word);
-	#				if($expr !~ /\$$word/) {
-	#					$expr =~ s/\b$word\b/$wordPl/g;
-	#				}
-	#				print "array: $word, $wordPl\n";
-	#			}
-				#print "array: $word, $wordPl\n";
-	#		}
-	#	}
-	
-	#	$body = $line; 
-	#	$body =~ s/.*:\s*//g;
-	#	chomp $body;
-	#	$body = meticOperator
-		
-
-	#	$string = "if";
-	#	if ($expr !~ m/(.*)/g) {
-	#		$string = $string." (".$expr.")";
-	#	} else {
-	#		$string = $string.$expr;
-	#	}
-		
-		
-		#print "$expr;\n";
-
-
-
-	#} elsif ($line =~ m/\(*[A-Z]+[0-9]*\s*\&|\||\^|\~|\<\<|\>\>\s*[A-Z]+[0-9]*\)*/gi) {
-
-		# This Section deals with bitwise operators
 
 }
 
---
+++ test00.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+
+#basic hello world print out.
+print "hello world"
